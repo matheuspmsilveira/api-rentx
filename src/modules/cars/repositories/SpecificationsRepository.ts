a4380691b5e59549f5998c1a966fa4b32892e363
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-10-01 15:00:02 -0300

    impl: useCase and controllers for categories and specification

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -2,7 +2,7 @@ import { Specification } from "../model/Specification";
 import {
   ICreateSpecificationDTO,
   ISpecificationsRepository,
-} from "./ISpecificationsRepository";
+} from "./implementations/ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
