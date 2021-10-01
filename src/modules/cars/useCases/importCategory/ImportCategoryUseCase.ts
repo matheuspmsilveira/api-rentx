8f5ca30c9a6906dc00c8e159d9248ef166edd7be
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-10-01 15:53:36 -0300

    impl: csv-parse to read file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  excute(file: any) {
-    console.log(file);
+  excute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
