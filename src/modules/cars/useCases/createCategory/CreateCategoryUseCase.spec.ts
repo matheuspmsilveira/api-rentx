432578b1502a8b44c89d3bb66fcd2c1421b88ca2
Matheus Silveira <matheuspmsilveira@gmail.com>
2022-01-03 15:10:36 -0300

    impl: test config

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Criar categoria", () => {
+  it("2 + 2 seja 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("2 + 2 não seja 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
