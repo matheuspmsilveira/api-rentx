c3da176e8cc18aad47ab5284d47368b82a3310e4
Matheus Silveira <matheuspmsilveira@gmail.com>
2022-01-07 16:12:16 -0300

    refac: app structure

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,8 @@
 import { EntityRepository, getRepository, Repository } from "typeorm";
 
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entities/User";
-import { IUsersRepository } from "../IUsersRepository";
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../../repositories/IUsersRepository";
+import { User } from "../entities/User";
 
 @EntityRepository(User)
 class UsersRepository implements IUsersRepository {
